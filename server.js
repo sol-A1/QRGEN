e8f11e71d8fc0c4bf024a493e15251f77d9ca1c4
sol-A1 <118661914+sol-A1@users.noreply.github.com>
2022-12-16 12:36:28 +0000

    Add files via upload

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const qr = require('qrcode');
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.post('/generate', (req, res) => {
+  const text = req.body.text;
+
+  qr.toDataURL(text, (err, url) => {
+    if (err) {
+      res.send('Error generating QR code');
+    } else {
+      res.send(`<img src="${url}"/>`);
+    }
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Server listening on port 3000');
+});
